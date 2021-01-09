347963487fe881d39f0a9f977982dc60063509b6
James <mail@jameshaughey.dev>
2021-01-09 13:39:17 +0000

    Remove manual backend

--- terraform/github-repos/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    dynamodb_table = "cirrius-backend-locks-global"
-    encrypt        = true
-    key            = "suspicious-tesla/terraform/github-repos/terraform.tfstate"
-    region         = "eu-west-1"
-    bucket         = "cirrius-backend-tfstate-global"
-  }
-}
